7d469b650872ebd93b0466d52ce4ed56845887d0
WoLFkni <wanglefan@sjtu.edu.cn>
2024-04-27 16:44:52 +0800

    vault backup: 2024-04-27 16:44:52

---
+++ learning/English/Note/note补录.md
@@ -0,0 +1,301 @@
+不懈的努力
+> relentless effort 
+> unrelenting effort 
+> persistent effort 
+> constant effort 
+> unyielding effort 
+> unswerving effort
+> ceaseless effort
+> unremitting effort
+
+十分重视
+> pay much heed to
+> attach much importance to
+> give top priority to
+> put premium on
+> highly value
+
+国策
+> state policy
+
+优惠政策
+> preferential policy
+
+提高……的意识
+> awake sb to sth
+> enhance one's awareness of environmental protection
+> raise one's awareness of
+> sharpen one's awareness of 
+
+增强竞争力
+> enhance competitiveness 
+> enhance competitive edge 
+> enhance competitive advantages
+	enhance + 抽象名词：魅力，竞争力，意识等
+
+专座
+> priority seats
+> courtesy seats
+
+老人
+> the senior citizens
+> the elderly 
+
+残疾人
+> the physically handicapped 
+> the physically disabled
+
+智障人士
+> the mentally-retarded
+> the mentally challenging
+
+孕妇
+> the pregnant
+
+总统专机
+> exclusive airplane
+
+独家专访
+> exclusive interview
+
+没能
+> deprive sb of sth
+> cost sb sth
+> strip sb of sth
+> deny sb sth
+> deny doing
+
+风景名声地
+> places of natural beauty
+> tourist attraction
+> tourist destination 
+> place of interest
+> scenic spot
+
+引起强烈反响，引起巨大轰动
+> have great repercussions
+> arouse a big sensation
+> make a splash
+> make a stir
+> create a furor
+
+遏制
+> contain the rapid spread of the covid-19 virus
+> curb the inflation / deflation 遏制 通货膨胀/通货紧缩
+
+病
+> infectious / contagious / catching diseases 传染病
+> non-infectious diseases
+> occupational diseases 职业病
+> chronic diseases 慢性病
+	 in chronological sequence 按时间顺序
+> acute diseases 急性病
+> sore throat 咽喉炎
+> social disease 性病
+> street walker 妓女
+> pedestrian street 步行街
+
+激烈的竞争
+> acute competition 
+> fierce competition
+> strong competition 
+> intense competition
+> tough competition
+> throat-cut competition
+> keen competition
+
+敏锐的听觉
+> acute hearing
+
+角
+> acute angle 锐角
+> vertical angle 直角
+> obtuse angle 钝角
+> horizontal angle 平角
+
+抑制房地产投机热
+> cool a craze of property speculation 
+	热潮（贬义）
+> frenzy 
+> fever
+> mania
+	热（褒义）
+> a hit
+> a boom
+
+发展
+> development
+> progress 
+> advancement
+> growth
+> strides
+
+老龄化社会
+> aging society
+	老龄化人口
+> aging population
+> greying population
+
+将会
+> be expected to
+> will 
+> be projected to 
+> be slated to 
+> look set to
+
+应对巨大挑战
+>	daunting 使人恐惧的
+> address the daunting challenges
+> embrace the challenges
+> rise up to the challenges
+> accept the challenges
+> meet the daunting challenges
+
+导致，引起
+> brought about
+> incurred
+	incurred: often negative
+> entailed
+> caused
+> spawned
+
+措施
+> feasible measures 可行的
+> remedial measures 补救的
+> emergent measures 紧急的
+> expedient measures 临时的，权益之计
+> drastic measures 严厉的
+> effective measures 有效的
+> strong measures 强有力的
+> adopt measures 采取措施
+
+缓解
+> ease the tension across the Taiwan Straight
+> alleviate the tooth ache
+> aggravate / worsen 加剧
+
+fuel
+> fuel crisis 燃料危机
+> fuel / arouse one's imagination / curiosity / interest 激发某人的想象力/好奇心/兴趣
+
+地址
+> temporary address 临时
+> permanent address 永久
+
+金融
+> stock wave 证券余波
+> financial crisis 金融危机
+> financial storm 金融风暴
+> financial turmoil / chaos / disturbance 金融动荡
+> stock market 证券市场
+> shareholder 股东
+> share speculation 炒股，股票投机
+> buffet 冲击
+> cut the budget / cut down on the budget
+
+一大群
+> a wave of tourist
+
+蜂拥而入
+> be flooding into
+> be swarming into 
+> be stampeding into
+> be crowding into
+> be flocking to
+
+如果没有
+> in the absence of sth
+	In the absence of WIFI, what would we do?
+
+调整，重整
+> overhaul
+
+编制预算
+> make a budget for the national defense
+
+退去，消退
+> ebb
+> the ebb and flow of tide 潮起潮落
+> the wax and wane of the moon 月盈月亏
+> the ups and downs of one's life 
+> the rises and falls of one's life 人生起伏
+
+偏离
+> stray away from 
+> deviate from
+> divorce from 
+	……which deviates from the traditional Asian values
+	日本偏离了传统的亚洲价值观念
+
+社会观念
+> social attitude
+
+迥然不同
+> be totally / sharply / vastly different from 
+
+cry
+> a far cry from 天壤之别
+> decry 谴责
+> outcry 强烈抗议，呐喊
+
+与……相似，酷似
+> be similar to
+> bear a striking resemblance to sth
+> be akin to sth
+
+引发
+> trigger
+> trigger much controversy引起广泛争议
+> prompt much controversy
+> arouse much controversy
+> fuel much controversy 
+> spark much controversy
+
+国际争端
+> international dispute
+
+许多
+> an array of 
+> a host of 
+> a battery of 
+> a parade of 
+> a multitude of 
+> a legion of 
+> an avalanche of 
+	+C(*pl*.)
+
+社会公害
+> social hazard 
+
+对……有害
+> be detrimental to 
+> be hazardous to
+> be pernicious to
+> be malicious to
+> exert a malign impact on 
+> do harm to
+> be harmful to
+> be deleterious to
+> have a deleterious effect on
+
+加速
+> speed up
+> accelerate
+> slow down 减速
+
+国际大都市
+> international metropolis
+> cosmopolitan city
+	international city 国际城市（没有主权，UN托管）
+
+维护国家主权
+> safeguard the sovereignty
+
+关注
+> be attentive to 
+> keep informed of 
+> show great concern for
+
+时事
+> the current events at home and broad
+> 
--- learning/cs/homework/assignment5/source/3_memory_allocator/main.cpp
+++ learning/cs/homework/assignment5/source/3_memory_allocator/main.cpp
@@ -1,7 +1,236 @@
 # include <iostream>
+# include <sstream>
+
 using namespace std;
+struct mem{
+    int x;
+    int y;
+    int a;
+    mem* next;
+};
+
+mem dummy;
+
+mem* head = &dummy;
+
+int s,n,m;
+
+mem* newmem(const int& a,const int& x,const int& y, mem* next){
+    mem* node = new mem;
+    (*node).a = a;
+    (*node).x = x;
+    (*node).y = y;
+    (*node).next = next;
+    return node;
+}
+
+void R(const int &a){
+    mem* p = dummy.next;
+    mem* current = &dummy;
+    while (p != nullptr){
+        if (p->a == a){
+            current->next = p->next;
+            delete p;
+            p = current->next;
+        }
+        else {
+            current = p;
+            p = current->next;
+        }
+    }
+}
+
+void S(){
+    mem* p = dummy.next;
+    mem* current = &dummy;
+    while (p != nullptr){
+        if (p->x > (current->x+current->y)){
+            (*p).x = current->x + current->y;
+        }
+        current = p;
+        p = current->next;
+    }
+}
+
+void A1(const int& a,const int& x,const int& y){
+    mem* p = dummy.next;
+    mem* current = &dummy;
+    if (p == nullptr){
+        p = newmem(a,x,y,nullptr);
+        dummy.next = p;
+        return;
+    }
+    while (p != nullptr && p->x < x+y){
+        p = p->next;
+        current = current->next;
+    }
+    if (current->x + current->y <= x && (x+y <= s)){
+        p = newmem(a,x,y,p);
+        (*current).next = p;
+        return;
+    }
+    else{
+        cout << "error" << endl;
+    }
+}
+
+void f(const int& a,const int& y){
+    mem* current = dummy.next;
+    if ((current->x) >= y){
+        mem* p = newmem(a,0,y,current);
+        dummy.next = p;
+        return;
+    }
+    if (current == nullptr){
+        mem* p = newmem(a,0,y,nullptr);
+        dummy.next = p;
+        return;
+    }
+    mem* p = current->next;
+    while (p != nullptr && (p->x - (current->x + current->y)) >= y){
+        p = newmem(a,(current->x + current->y),y,p);
+        (*current).next = p;
+        return;
+    }
+    if (current->x+current->y+y <= s){
+        p = newmem(a,(current->x + current->y),y,p);
+        (*current).next = p;
+        return;
+    }
+    else {
+        cout << "error" << endl;
+    }
+}
+
+void b(const int& a,const int& y){
+    mem* current = dummy.next;
+    mem* q = &dummy;
+    int k = 0;
+    if (current == nullptr){
+        mem* p = newmem(a,0,y,nullptr);
+        (*q).next = p;
+        return;
+    }
+    if (current->x >= y){
+        k = current->x;
+    }
+    mem* p = current->next;
+    while (p != nullptr){
+        if (p->x - (current->x + current->y) > k){
+            q = current;
+            k = p->x - (current->x + current->y);
+        }
+        current = p;
+        p = current->next;
+    }
+    if (k < y){
+        if ((current->x+current->y + y) <= s){
+            p = newmem(a,(current->x + current->y),y,p);
+            (*current).next = p;
+            return;
+        }
+        cout << "error" << endl;
+    }else{
+        mem* qnext = newmem(a,q->x+q->y,y,q->next);
+        (*q).next = qnext;
+    }
+}
+
+void smallest(const int& a,const int& y){
+    mem* current = dummy.next;
+    mem* q = &dummy;
+    int k = 0;
+    if (current == nullptr){
+        mem* p = newmem(a,0,y,nullptr);
+        (*q).next = p;
+        return;
+    }
+    if (current->x >= y){
+        k = current->x;
+    }
+    mem* p = current->next;
+    while (p != nullptr){
+        if (k == 0 && (p->x - (current->x + current->y) >= y)){
+            q = current;
+            k = p->x - (current->x + current->y);
+        }
+        else if (p->x - (current->x + current->y) >= y && (p->x - (current->x + current->y)) < k){
+            q = current;
+            k = p->x - (current->x + current->y);
+        }
+        current = p;
+        p = current->next;
+    }
+    if (k < y){
+        if ((current->x+current->y + y) <= s){
+            p = newmem(a,(current->x + current->y),y,p);
+            (*current).next = p;
+            return;
+        }
+        cout << "error" << endl;
+    }else{
+        mem* qnext = newmem(a,q->x+q->y,y,q->next);
+        (*q).next = qnext;
+    }
+}
+
+void A2(const int& a,const int& y,const char& c){
+    if (c == 'f'){
+        f(a,y);
+    }
+    else if (c == 'b'){
+        b(a,y);
+    }
+    else {
+        smallest(a,y);
+    }
+}
+
+void O(){
+     mem* p = dummy.next;
+     while (p != nullptr){
+        cout << p->x << " " << p->y  << " " << p->a << endl;
+        p = p->next;
+     }
+}
+
+void op(){
+    string str;
+    getline(cin, str);
+    istringstream iss(str);
+    string ope;
+    iss >> ope;
+    if (ope == "R"){
+        int a;
+        iss >> a;
+        R(a);
+    }
+    else if (ope == "S"){
+        S();
+    }
+    else if (ope == "A1"){
+        int a,x,y;
+        iss >> a >> x >> y;
+        A1(a,x,y);
+    }
+    else if (ope == "A2"){
+        int a,y;
+        char c;
+        iss >> a >> y >> c;
+        A2(a,y,c);
+    }
+    else if (ope == "O"){
+        O();
+    }
+}
 
 int main()
 {
+    cin >> s >> n >> m;
+    cin.ignore();
+    dummy.next = nullptr;
+    for (int i = 0; i < m; i++){
+        op();
+    }
     return 0;
 }
